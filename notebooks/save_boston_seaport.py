cea6d25ab200dbe3b26518fdabec6f0fd2af232b
Umair <khawaja.umair@tum.de>
2024-08-30 12:36:58 +0000

    update boston seaport labelling script

--- notebooks/save_boston_seaport.py
+++ notebooks/save_boston_seaport.py
@@ -1,12 +1,13 @@
 import repackage
 repackage.up()
-import concurrent.futures
-import open3d as o3d
+
 from datasets.nuscenes import NuScenesMultipleRadarMultiSweeps
 from utils.occupancy import *
 from utils.labelling import *
 from utils.transforms import *
 from utils.postprocessing import *
+from utils.ransac_solver import RANSACSolver
+from utils.motion_estimation import *
 from autolabeler import AutoLabeler
 import pandas as pd
 import os.path as osp
@@ -17,13 +18,37 @@ from nuscenes import NuScenes
 from multiprocessing import cpu_count, Pool, set_start_method
 import matplotlib
 import matplotlib.pyplot as plt
+from utils.visualization import map_pointcloud_to_image, render_pointcloud_in_image, plot_maps
+from utils.motion_estimation import remove_dynamic_points
 matplotlib.use('Agg')
 plt.ioff()
+import concurrent.futures
+# from multiprocess.pool import Pool
 
-NUM_WORKERS = min(cpu_count(), 4)  # Adjust this number based on your machine's capabilities
-DF_PATH = '../sps_nuscenes_more_matches_df.json'
+
+
+NUM_WORKERS = min(cpu_count(), 12)  # Adjust this number based on your machine's capabilities
+DF_PATH = '../sps_nuscenes_more_matches_df.json' # Sticking to this since odom/loc benchmarking was done on this to compare experiments
 sps_df = pd.read_json(DF_PATH)
 
+
+DATA_DIR = "/shared/data/nuScenes/"
+versions = {'trainval': 'v1.0-trainval', 'test': 'v1.0-test'}
+nuscenes_exp = {
+    vname: NuScenes(dataroot=DATA_DIR, version=version, verbose=False)
+    for vname, version in versions.items()
+}
+
+
+REF_FRAME = None
+REF_SENSOR = None
+FILTER_POINTS = False
+
+NUM_SWEEPS = 5
+APPLY_DPR = True
+DPR_THRESH = 0.15
+OCTOMAP_RESOLUTION = 0.15 # For dividing space, for lidar 0.1 is suitable but since radar is sparse a larger value might be better
+VOXEL_SIZE = 0.01
 
 ICP_FILTERING = True
 SEARCH_IN_RADIUS = True
@@ -36,17 +61,6 @@ FILTER_OUT_OF_BOUNDS = False
 USE_COMBINED_MAP = False
 
 
-
-ref_frame = 'global'
-num_sweeps = 5
-ref_sensor = 'RADAR_FRONT'
-apply_dpr = False
-filter_points = False
-dpr_thresh = 0.0
-voxel_size = 0.1
-
-
-DATA_DIR = "/shared/data/nuScenes/"
 SENSORS = ["RADAR_FRONT", "RADAR_FRONT_LEFT", "RADAR_FRONT_RIGHT", "RADAR_BACK_LEFT", "RADAR_BACK_RIGHT"]
 
 versions = {'trainval': 'v1.0-trainval', 'test': 'v1.0-test'}
@@ -68,12 +82,13 @@ def process_row(row):
         nusc=nuscenes_exp[ref_split],
         sequence=int(ref_scene_name.split("-")[-1]),
         sensors=SENSORS,
-        nsweeps=num_sweeps,
-        ref_frame=ref_frame,
-        ref_sensor=ref_sensor,
-        apply_dpr=apply_dpr,
-        filter_points=filter_points,
-        ransac_threshold=dpr_thresh
+        nsweeps=NUM_SWEEPS,
+        ref_frame=REF_FRAME,
+        ref_sensor=REF_SENSOR,
+        apply_dpr=False,
+        filter_points=FILTER_POINTS,
+        ransac_threshold=-1,
+        reformat_pcl=False
     )}
 
     for matched_scene, data in closest_scenes.items():
@@ -82,31 +97,55 @@ def process_row(row):
             nusc=nuscenes_exp[data['split']],
             sequence=int(matched_scene.split("-")[-1]),
             sensors=SENSORS,
-            nsweeps=num_sweeps,
-            ref_frame=ref_frame,
-            ref_sensor=ref_sensor,
-            apply_dpr=apply_dpr,
-            filter_points=filter_points,
-            ransac_threshold=dpr_thresh
+            nsweeps=NUM_SWEEPS,
+            ref_frame=REF_FRAME,
+            ref_sensor=REF_SENSOR,
+            apply_dpr=False,
+            filter_points=FILTER_POINTS,
+            ransac_threshold=-1,
+            reformat_pcl=False
         )
 
     # Process and downsample point clouds
-    pointclouds = {name : [dl[i][0] for i in range(dl.num_readings)] for name, dl in row_dls.items()}
-    maps = {name : np.vstack(pcl) for name, pcl in pointclouds.items()}
+    scene_pointclouds = {name : [dl[i][0] for i in range(dl.num_readings)] for name,dl in dataloaders.items()}
+    scene_calibs = {name : [dl[i][1] for i in range(dl.num_readings)] for name,dl in dataloaders.items()}
+    scene_poses = {name: dl.global_poses for name,dl in dataloaders.items()}
+
     # downsampled_pointclouds = {
     #     name: o3d.geometry.PointCloud(o3d.utility.Vector3dVector(pcls[:,:3])).voxel_down_sample(voxel_size=0.05).points
     #     for name, pcls in maps.items()
     # }
-    
     # Consolidate downsampled point clouds
     # scene_map = {name: pcls for name, pcls in downsampled_pointclouds.items()}
-    scene_map = maps
-    scene_pose = {name: dl.global_poses for name, dl in row_dls.items()}
+
+    ## Applying DPR
+    scene_pointclouds_dpr_masks, global_scene_pointclouds = remove_dynamic_points(scene_pointclouds, scene_calibs, scene_poses, SENSORS, filter_sensors=True, dpr_thresh=DPR_THRESH, save_vis=False)
+
+    scene_dpr_masks = {name : np.hstack(masks) for name,masks in scene_pointclouds_dpr_masks.items()}
+    scene_maps = {name : np.vstack(pcls) for name,pcls in global_scene_pointclouds.items()}
+
+    static_scene_maps = {}
+    dynamic_scene_maps = {}
+    dpr_sps_maps = {}
+
+    for name in scene_maps:
+        indices = scene_dpr_masks[name]
+        map_pcl = scene_maps[name]
+        filtered_map_pcl = map_pcl[indices]
+        static_scene_maps[name] = filtered_map_pcl
+        dynamic_scene_maps[name] = map_pcl[~indices]
+
+        dpr_sps_pcl = np.hstack((map_pcl, np.array(indices).astype(np.int).reshape(-1, 1)))
+        dpr_sps_maps[name] = dpr_sps_pcl
+
+    dynamic_scene_maps = filter_maps_icp(dynamic_scene_maps, alignment_thresh=0.5, overlapping_thresh=0.25)
+
     
-    return pointclouds, scene_map, scene_pose, row_dls
+    return global_scene_pointclouds, static_scene_maps, dynamic_scene_maps, scene_poses, row_dls
 
 # Parallel processing using ThreadPoolExecutor
 scene_pointclouds = {}
+dynamic_scene_maps = {}
 scene_maps = {}
 scene_poses = {}
 dataloaders = {}
@@ -117,11 +156,12 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
     for future in concurrent.futures.as_completed(future_to_row):
         i = future_to_row[future]
         # try:
-        pointclouds, scene_map, scene_pose, row_dls = future.result()
+        pointclouds, scene_map, dynamic_scene_map, scene_pose, row_dls = future.result()
         # If using downsampled o3d pcl
         # scene_map = {name: np.asarray(o3d_map) for name, o3d_map in scene_map.items()}
         scene_pointclouds.update(pointclouds)
         scene_maps.update(scene_map)
+        dynamic_scene_maps.update(dynamic_scene_map)
         scene_poses.update(scene_pose)
         dataloaders.update(row_dls)
     # except Exception as e:
@@ -129,41 +169,12 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
 
 if FILTER_BY_POSES:
     scene_maps, scene_poses = create_filtered_maps(scene_poses, scene_pointclouds, threshold=1)
-elif FILTER_BY_RADIUS:
+
+if FILTER_BY_RADIUS:
     scene_maps, scene_poses = create_filtered_maps_with_radius(scene_poses, scene_pointclouds, threshold=1)
 
-
 if ICP_FILTERING:
-    pcd_dict = {key: convert_to_open3d_pcd(val) for key, val in scene_maps.items()}
-    pcd_merged = o3d.geometry.PointCloud()
-    for pcd in pcd_dict.values():
-        pcd_merged += pcd
-
-    keys = list(pcd_dict.keys())
-    transforms = {}
-    for i in range(len(keys)):
-        for j in range(i + 1, len(keys)):
-            trans_key = f"{keys[i]}_{keys[j]}"
-            transforms[trans_key] = align_pointclouds(pcd_dict[keys[i]], pcd_dict[keys[j]])
-
-    for key, trans in transforms.items():
-        src, tgt = key.split('_')
-        pcd_dict[src].transform(trans)
-
-    overlapping_indices = find_overlapping_points(pcd_merged)
-
-    cropped_pcd_dict = {}
-    cropped_indices_dict = {}
-    for key, pcd in pcd_dict.items():
-        original_indices = get_original_indices(pcd_merged, pcd, overlapping_indices)
-        overlapping_points = np.asarray(pcd.points)[list(original_indices), :]
-        cropped_pcd = o3d.geometry.PointCloud()
-        cropped_pcd.points = o3d.utility.Vector3dVector(overlapping_points)
-        cropped_pcd_dict[key] = cropped_pcd
-        cropped_indices_dict[key] = list(original_indices)
-
-    cropped_scene_maps = {key: np.asarray(val.points) for key, val in cropped_pcd_dict.items()}
-    scene_maps = cropped_scene_maps
+    scene_maps = filter_maps_icp(scene_maps)
 
 if USE_LIDAR_LABELS:
     lidar_labels = {name: np.load(f'inputs/lidar_labels/{name}_lidar_labels.npy') for name, map in scene_maps.items()}
@@ -171,28 +182,49 @@ else:
     lidar_labels = None
 
 if USE_OCCUPANCY_PRIORS:
-    scene_voxel_maps = {name: create_voxel_map(scans[:,:3], voxel_size) for name, scans in scene_pointclouds.items()}
+    scene_scans = {name: {i:scene_pointclouds[name][i][:,:3] for i in range(len(dataloader))} for name,dataloader in dataloaders.items()}
+    scene_voxel_maps = {name: create_voxel_map(scans, VOXEL_SIZE) for name, scans in scene_scans.items()}
 else:
     scene_voxel_maps = None
 
-scene_octomaps = {name: build_octomap(dl) for name, dl in dataloaders.items()}
+scene_octomaps = {name: build_octomap(dl, resolution=OCTOMAP_RESOLUTION) for name,dl in dataloaders.items()}
+
 
 sps_labeler = AutoLabeler(
     scene_maps=scene_maps, ref_map_id=list(scene_poses.keys())[0], scene_poses=scene_poses,
     scene_octomaps=scene_octomaps, lidar_labels=lidar_labels,
     dynamic_priors=scene_voxel_maps, use_octomaps=True,
     search_in_radius=SEARCH_IN_RADIUS, radius=RADIUS, use_combined_map=USE_COMBINED_MAP,
-    downsample=True, voxel_size=voxel_size, filter_out_of_bounds=FILTER_OUT_OF_BOUNDS
+    downsample=True, voxel_size=VOXEL_SIZE, filter_out_of_bounds=FILTER_OUT_OF_BOUNDS
 )
 
 sps_labeler.label_maps()
 
 
+complete_sps_labelled_map = sps_labeler.labeled_environment_map
 
-lmap = sps_labeler.labeled_environment_map
-points = lmap[:, :3]
-stable_probs = lmap[:,-1]
-filename = f"boston-seaport_sps_nodpr.asc"
+dynamic_sps_scene_points = {}
+for name in dynamic_scene_maps:
+    dyn_sps_pcl = np.hstack((dynamic_scene_maps[name][:,:3], np.zeros(len(dynamic_scene_maps[name])).astype(np.int).reshape(-1, 1)))
+    dynamic_sps_scene_points[name] = dyn_sps_pcl
+
+
+for scene_name, dyn_points in dynamic_sps_scene_points.items():
+    complete_sps_labelled_map = np.vstack([complete_sps_labelled_map, dyn_points])
+
+
+labeled_map = complete_sps_labelled_map
+points = labeled_map[:, :3]
+stable_probs = labeled_map[:, -1]
+
+plt.figure(figsize=(10, 10))
+plt.scatter(points[:, 0], points[:, 1], c=stable_probs, cmap='RdYlGn', s=0.05)
+plt.colorbar(label='Stability')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.savefig(f'boston-seaport_sps.jpg', dpi=300)
+
+filename = f"boston-seaport_sps.asc"
 np.savetxt(filename,
             np.hstack([points,stable_probs.reshape(-1,1)]),
             fmt='%.6f', delimiter=' ',
